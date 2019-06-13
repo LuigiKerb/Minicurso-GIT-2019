0df29f8924acb4a1405d6361460effe8f100b7dc
Luigi Miranda <luigi.miranda75@gmail.com>
2019-06-13 20:30:46 -0300

    Adicionando função de Dividir corrigida

--- projeto.c
+++ projeto.c
@@ -12,9 +12,14 @@ int multipli(int a, int b){
 	return a*b;
 }
 
+float dividir(int a, int b){
+	return a/b;
+}
+
 int main(){
 	printf("Somando dois numeros: %d\n", soma(2,3));
 	printf("Subtraindo dois numeros: %d\n", subtracao(5,2));
 	printf("Multiplicando dois numeros: %d\n", multipli(5,2));
+	printf("Dividindo dois numeros: %.2f\n", dividir(10,3));
 	return 0;
 }
